cd11e6930f36be31a225bcb295a5022c485551c1
James Bostock <77472194+james-bostock-cx@users.noreply.github.com>
2023-08-24 12:40:07 +1000

    Update main.py

--- src/main.py
+++ src/main.py
@@ -3,5 +3,9 @@ import sys
 
 # A primitive rm(1).
 if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print(f'usage: python3 ${sys.argv[0]} <files>', file=sys.stdout)
+        sys.exit(1)
+
     for arg in sys.argv[1:]:
         os.unlink(arg)
